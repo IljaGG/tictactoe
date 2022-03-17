051e1a37044f8bcbd2cf006ca6220df1a58deff6
Ilja Gaus-Gerbeth <ilja@Iljas-MacBook-Air.local>
2022-03-17 23:18:30 +0100

    last two bugs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let fields = [];
-
+let gameOver = false;
 let currentShape = "cross";
 
 
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
         if (currentShape == "cross") {
             currentShape = "circle";
             document.getElementById('player-1').classList.add('player-inactive');
@@ -72,6 +72,27 @@ function checkForWin() {
 
     if (winner) {
         console.log('GEWONNEN', winner);
+        gameOver = true;
+        setTimeout(function() {
+        document.getElementById('game-over').classList.remove('d-none');
+        document.getElementById('restart-btn').classList.remove('d-none');
+    }, 1000);
     }
 
+}
+
+function restart() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+
+    for (let i = 1; i < 8; i++) {
+        document.getElementById('line-' + i).classList.add('d-none');
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('circle-' + i).classList.add('d-none');
+        document.getElementById('cross-' + i).classList.add('d-none');
+    }
 }
